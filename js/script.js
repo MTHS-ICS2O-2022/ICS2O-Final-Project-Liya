03619abcba3ffb55d60143d7b2d0c850558c741b
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-01-16 19:48:06 +0000

    background

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 liya getachew All rights reserved
+//
+// Created by: liya getachew
+// Created on: Jan 2023
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-05-JS/sw.js", {
+    scope: "/ICS2O-Unit5-05-JS/",
+  })
+}
+
+

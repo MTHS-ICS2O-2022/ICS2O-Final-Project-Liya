81dbab892ad744a822300d3cb8ddbb3521cc59d2
Liya-Getachew <Liya-Getachew@users.noreply.github.com>
2023-01-20 19:34:05 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 // Created on: Jan 2023
 // This file contains the JS functions for index.html
 
-"use strict"
+"use strict";
 
 /**
  * Check service worker.
@@ -12,33 +12,32 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Unit5-05-JS/sw.js", {
     scope: "/ICS2O-Unit5-05-JS/",
-  })
+  });
 }
 
 // functions for the quiz.
 
 function submitQuiz() {
-  document.getElementById("container").style.display = "none"
-  document.getElementById("results").style.display = "block"
-  console.log("end screen")
+  document.getElementById("container").style.display = "none";
+  document.getElementById("results").style.display = "block";
+  console.log("end screen");
 
-  let score = 0
-  const userAnswer2 = parseInt(document.getElementById("sample2").value)
-  const userAnswer3 = parseInt(document.getElementById("question3").value)
-  const userAnswer4 = parseInt(document.getElementById("question4").value)
+  let score = 0;
+  const userAnswer2 = parseInt(document.getElementById("sample2").value);
+  const userAnswer3 = parseInt(document.getElementById("question3").value);
+  const userAnswer4 = parseInt(document.getElementById("question4").value);
 
   if (userAnswer2 == -4) {
-    score++
+    score++;
   }
 
   if (userAnswer3 == 4) {
-    score++
+    score++;
   }
 
   if (userAnswer4 == 1) {
-    score++
+    score++;
   }
-    
-  document.getElementById("score").innerHTML = score
-  
+
+  document.getElementById("score").innerHTML = score;
 }

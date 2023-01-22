28eeca862ea7fdc13d77542fb5df399f6bd5e2e9
Liya-Getachew <Liya-Getachew@users.noreply.github.com>
2023-01-22 14:00:51 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 // Created on: Jan 2023
 // This file contains the JS functions for index.html
 
-"use strict";
+"use strict"
 
 /**
  * Check service worker.
@@ -12,46 +12,46 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Final-Project-Liya/sw.js", {
     scope: "/ICS2O-Final-Project-Liya/",
-  });
+  })
 }
 
 // functions for the quiz.
 
 function submitQuiz() {
-  document.getElementById("container").style.display = "none";
-  document.getElementById("results").style.display = "block";
-  console.log("end screen");
+  document.getElementById("container").style.display = "none"
+  document.getElementById("results").style.display = "block"
+  console.log("end screen")
 
-  let score = 0;
-  const userAnswer1 = parseInt(document.getElementById("question-one").value);
-  const userAnswer2 = parseInt(document.getElementById("question-two").value);
-  const userAnswer3 = parseInt(document.getElementById("question-three").value);
-  const userAnswer4 = parseInt(document.getElementById("question-four").value);
-  const userAnswer5 = parseInt(document.getElementById("question-five").value);
-  const userAnswer6 = parseInt(document.getElementById("question-six").value);
+  let score = 0
+  const userAnswer1 = parseInt(document.getElementById("question-one").value)
+  const userAnswer2 = parseInt(document.getElementById("question-two").value)
+  const userAnswer3 = parseInt(document.getElementById("question-three").value)
+  const userAnswer4 = parseInt(document.getElementById("question-four").value)
+  const userAnswer5 = parseInt(document.getElementById("question-five").value)
+  const userAnswer6 = parseInt(document.getElementById("question-six").value)
 
   if (userAnswer1 == 4) {
-    score++;
+    score++
   }
 
   if (userAnswer2 == -4) {
-    score++;
+    score++
   }
 
   if (userAnswer3 == 4) {
-    score++;
+    score++
   }
 
   if (userAnswer4 == 1) {
-    score++;
+    score++
   }
 
   if (userAnswer5 == 9) {
-    score++;
+    score++
   }
 
   if (userAnswer6 == 55) {
-    score++;
+    score++
   }
 
   let grade = [
@@ -60,7 +60,7 @@ function submitQuiz() {
     "./images/c+.svg",
     "./images/d+.svg",
     "./images/disappointed.svg",
-  ];
+  ]
 
   let message = [
     "Congrats! You did it! You beat the quiz and proved that you <strong>can</strong> do math.",
@@ -68,27 +68,27 @@ function submitQuiz() {
     "It's ok, you can do better next time.",
     "One day.",
     "...sorry but...did you even try?",
-  ];
+  ]
 
-  let objNumber = 0;
+  let objNumber = 0
 
   if (score == 6) {
-    objNumber = 0;
+    objNumber = 0
   } else if (score == 5) {
-    objNumber = 1;
+    objNumber = 1
   } else if (score == 4) {
-    objNumber = 2;
+    objNumber = 2
   } else if (score == 3 || score == 2 || score == 1) {
-    objNumber = 3;
+    objNumber = 3
   } else {
-    objNumber = 4;
+    objNumber = 4
   }
 
   document.getElementById("score").innerHTML =
-    "Your Final Mark is: " + score + "/6";
+    "Your Final Mark is: " + score + "/6"
   document.getElementById("final-mark").innerHTML =
     "<img src='" +
     grade[objNumber] +
-    "' alt='final-mark' height='200' width='300'>";
-  document.getElementById("messages").innerHTML = "<br>" + message[objNumber];
+    "' alt='final-mark' height='200' width='300'>"
+  document.getElementById("messages").innerHTML = "<br>" + message[objNumber]
 }
